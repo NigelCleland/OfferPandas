600f3eb45285d482c5e59931b83299ba687a31c2
Nigel Cleland <nigel.cleland@gmail.com>
2014-08-27 20:31:14 +1200

    Adding napoleon dependency

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ import OfferPandas
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'numpydoc',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinxcontrib.napoleon',
               'sphinx.ext.pngmath']
 
 # Add any paths that contain templates here, relative to this directory.
